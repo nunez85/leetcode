dd84fa7d596d5300de4e2a76a91bc439ffcd37e0
Isaac <inunez.dev@gmail.com>
2023-11-09 18:35:35 -0700

    Added more comments to productExceptSelf

--- product-of-array-except-self/productExceptSelf.py
+++ product-of-array-except-self/productExceptSelf.py
@@ -1,7 +1,9 @@
 # Given an integer array nums, return an array answer such that answer[i] is
 # equal to the product of all the elements of nums except nums[i].
-# Input [1, 2, 3, 4]
-# Output [24, 12, 8, 6]
+# Input   [1,  2,  3,  4]
+# prefix  [1,  2,  6,  24]
+# postfix [24, 24, 12, 4]
+# Output  [24, 12, 8,  6]
 def productExceptSelfBruteForce(nums: list[int]) -> list[int]:
     x = 0
     y = 0
